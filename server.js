2752c0da2aceb5c13f9a182a7a1c1b7fe83d9a8d
akash301994 <patel.akash30@icloud.com>
2023-09-12 14:16:17 -0400

    imported all frameworks, initiallzed express, and added basic middleware

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
+
+// importing frameworks 
 const express = require('express');
 const path = require('path');
-const PORT = process.env.PORT || 3001;
-const notesDb = require('./Develop/db/db.json');
 
+//initialzing express
 const app = express();
+const notesdb = require('./db/db.json');
+const id = require('uuid').v4;
+const PORT = process.env.PORT || 3001;
+
+
+//using middleware to parse incoming requests then config to json data.
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
 
