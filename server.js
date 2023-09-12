2f00672b1de98aa0e9afb68ca42c0682ec7281cc
akash301994 <patel.akash30@icloud.com>
2023-09-12 14:26:43 -0400

    added the rest of the routes

--- server.js
+++ server.js
@@ -17,3 +17,42 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
+
+//starting routes
+app.get('/api/notes', (req, res) => res.json(notesdb));
+
+app.post('/api/notes', (req, res) => {
+  const newNoteTitle = req.body.title;
+  const newNoteText = req.body.text;
+
+  const newNote = { 
+    title: newNoteTitle, 
+    text: newNoteText
+  };
+
+  newNote.id = id();
+  notesdb.push(newNote);
+  res.json(newNote);
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+  const id = req.params.id;
+  const note = notesdb.find(note => note.id === id);
+  const index = notesdb.indexOf(note);
+  notesdb.splice(index, 1);
+  res.json(notesdb);
+});
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+
+//console logging when port is started
+app.listen(PORT, () => {
+  console.log(`App listening at http://localhost:${PORT}`);
+});
